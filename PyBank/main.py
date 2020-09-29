e4dca5628a32fa76dc3bafb1ea743c3954715ce3
Efritch <70618234+Efritch@users.noreply.github.com>
2020-09-29 00:14:10 -0400

    Added code to determine the monthly budget differences.

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@
 
 import os
 import csv
+import numpy as np
 
 # Set path for file
 csv_budget = os.path.join("..", "Resources", "budget_data.csv")
@@ -9,6 +10,7 @@ csv_budget = os.path.join("..", "Resources", "budget_data.csv")
 # Lists to store data
 day = []
 budget_amount = []
+monthly_difference = []
 
 # Open the CSV
 with open(csv_budget) as budget_data:
@@ -16,7 +18,7 @@ with open(csv_budget) as budget_data:
 
     # Reads & prints header row in CSV file
     csv_header = next(csv_reader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
     
     total_profit_loss = 0
 
@@ -29,6 +31,7 @@ with open(csv_budget) as budget_data:
 
     # print(budget_amount)
 
+# Finds the total number of months in the data file
 total_num_months = len(day)
 
 print(total_num_months)
@@ -37,6 +40,13 @@ for x in budget_amount:
         total_profit_loss = total_profit_loss + int(x)
     # return total_profit_loss
 
-
 print(total_profit_loss)
+#print(budget_amount)
+
+# Find monthly budget changes
+for i in range(1, len(budget_amount)):
+    monthly_difference.append(int(budget_amount[i]) - int(budget_amount[i - 1]))
+
+print("monthly difference: ", str(monthly_difference))
+
 
