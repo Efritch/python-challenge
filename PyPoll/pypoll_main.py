ca40fe2ab8020a61ba6c1ed99d28440bc5acc20c
Efritch <70618234+Efritch@users.noreply.github.com>
2020-09-29 12:55:45 -0400

    Added code to calculate total votes, percentages, and determine the election winner

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -2,7 +2,7 @@
 
 import os
 import csv
-import statistics
+import operator
 
 # Lists to store data
 voters = []
@@ -35,15 +35,15 @@ with open(csv_poll_data) as voting_data:
 
 # Find the total number of votes.
 total_votes = len(voters)
-print(total_votes)
+
 
 # Create a list of unique counties.
 county_list = set(county)
-print(county_list)
+
 
 # Create a list of unique candidates.
 candidate_list = set(candidate)
-print(candidate_list)
+
 
 # Create a dictionary for all lists
 candidate_data = dict(zip(voters,candidate))
@@ -61,8 +61,46 @@ for x in candidate_data:
         otooley_vote_count.append(candidate_data[x])
   
 
-print(len(khan_vote_count))
-print(len(correy_vote_count))
-print(len(li_vote_count))
-print(len(otooley_vote_count))
+votes_for_khan = len(khan_vote_count)
+votes_for_correy = len(correy_vote_count)
+votes_for_li = len(li_vote_count)
+votes_for_otooley = len(otooley_vote_count)
 
+khan_vote_pct = "{:.3%}".format(votes_for_khan / total_votes)
+correy_vote_pct = "{:.3%}".format(votes_for_correy / total_votes)
+li_vote_pct = "{:.3%}".format(votes_for_li / total_votes)
+otooley_vote_pct = "{:.3%}".format(votes_for_otooley / total_votes)
+
+
+# Create a dictionary that lists the candidate and their total vote counts.
+vote_summary = {"Khan": [votes_for_khan], "Correy": [votes_for_correy], "Li": [votes_for_li], "O'Tooley": [votes_for_otooley]}
+
+winner = max(vote_summary,key = vote_summary.get)
+
+
+# Prints the output in the terminal
+print("")
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------")
+print(f"Khan: {khan_vote_pct}: ({votes_for_khan})")
+print(f"Correy: {correy_vote_pct}: ({votes_for_correy})")
+print(f"Li: {li_vote_pct}: ({votes_for_li})")
+print(f"O'Tooley: {otooley_vote_pct}: ({votes_for_otooley})")
+print("----------------------------")
+print(f"Winner: {winner}")
+print("----------------------------")
+
+
+#File path to write to
+#output_path = os.path.join("..", "Analysis", "pybank_results.csv")
+
+#Open the file using "write" mode. Specify the variable to hold the contents
+#with open(output_path, "w") as csvfile:
+
+#    csvwriter = csv.writer(csvfile, delimiter=',')
+
+#    csvwriter.writerow(["Financial Analysis"])
+#    csvwriter.writerow(["Total Months", "Total", "Average Change", "Greatest Increase Month", "Greatest Increase in Profits", "Greatest Decrease Month", "Greatest Decrease in Profits"])
+#    csvwriter.writerow([total_num_months, total_profit_loss, rounded_average, max_month, max_profit_loss_change, min_month, min_profit_loss_change])
